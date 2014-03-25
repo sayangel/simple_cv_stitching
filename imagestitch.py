eccdcf0378c7ddee3fab855848281f237210ac33
sayangel <sayangel@gmail.com>
2014-03-25 11:34:24 -0400

    added alpha param to stitching

--- imagestitch.py
+++ imagestitch.py
@@ -56,7 +56,7 @@ def open_images(img_array):
 
 #stitch_images will stitch a left/right image by default
 #pass vertical and horizontal parameters as needed to stitch images
-def stitch_images(image1, image2, vertical=False, horizontal=True):
+def stitch_images(image1, image2, vertical=False, horizontal=True, transparency = 1.0):
   #vertical/horizontal dimension multiplier
   v = 1
   h = 1
@@ -119,7 +119,7 @@ def stitch_images(image1, image2, vertical=False, horizontal=True):
   #           , colorSpace=ColorSpace.RGB
   #           ).toBGR()
 
-  stitched_image = stitched_image.blit(image1, alpha=0.5)
+  stitched_image = stitched_image.blit(image1, alpha=transparency)
   return stitched_image
 
 
@@ -143,8 +143,8 @@ if multiple:
 vertical = False
 horizontal = False
 
-#figure out orientation of stitching if only 2 images to determine dimensions
 if not multiple:
+  #figure out orientation of stitching if only 2 images to determine dimensions
   orientation = raw_input("Vertical (y/n): ")
   if orientation == "y" or orientation == "Y" or orientation == "yes" or orientation == "Yes":
     vertical = True
@@ -166,62 +166,7 @@ if multiple:
 
   print "Stitching bottom right to rest of image..."
   #(3) stitch above image with bottom left corner - no vertical no horizontal
-  final = stitch_images(second_stitch, input_img_list[3], False, False)
+  final = stitch_images(second_stitch, input_img_list[3], False, False, 0.5)
 
   final.save("stitched.jpg")
   print "Image saved as stitched.jpg"
-
-
-# #create destination image
-# #ASSUMPTION: img1 and img2 have the same dimensions
-# dst = Image((img1.width*horizontal, img1.height*vertical))
-#
-# print "Running findKeypointMatch..."
-
-# # Find the keypoints.
-# match_features = img1.findKeypointMatch(img2)
-#
-# # The homography matrix.
-# homo = match_features[0].getHomography()
-# #Uncomment if want to prevent perspective shift
-# # homo[2][0] = 0
-# # homo[2][1] = 0
-# # homo[2][2] = 1
-#
-# #only 2D translation and rotation.. I think
-# affine_transform = np.array([homo[0], homo[1]])
-#
-# dst_mat = dst.getMatrix()
-#
-# # transform the image.
-# # should crop out the overlapping parts to avoid weird darkening etc.
-# # Image constructor: Image(source=None, camera=None, colorSpace=0, verbose=True, sample=False, cv2image=False, webp=False)
-# # cv2.warpPerspective(src, M, dsize[, dst[, flags[, borderMode[, borderValue]]]])
-#
-# #warpPerspective Implementation
-# x = Image(
-#           #consider using warpAffine here since we can assume no 3D perspective change
-#           cv2.warpPerspective(
-#                               np.array((img2.getMatrix()))
-#                               , homo
-#                               , (dst_mat.rows, dst_mat.cols+300)
-#                               , np.array(dst_mat)
-#                               , cv2.INTER_CUBIC
-#                               )
-#           , colorSpace=ColorSpace.RGB
-#           ).toBGR()
-#
-# # x = Image(
-# #           #consider using warpAffine here since we can assume no 3D perspective change
-# #           cv2.warpAffine(
-# #                               np.array((img2.getMatrix()))
-# #                               , affine_transform
-# #                               , (dst_mat.rows, dst_mat.cols+300)
-# #                               , np.array(dst_mat)
-# #                               , cv2.INTER_CUBIC
-# #                               )
-# #           , colorSpace=ColorSpace.RGB
-# #           ).toBGR()
-#
-# # blit the img1 now on coordinate (0, 0).
-# x = x.blit(img1, alpha=0.5)
